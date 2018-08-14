862dee2a5f2925f7cdd691b2f2e21f12d4f152ea
huzhongyu <noahahahah@foxmail.com>
2018-08-14 21:39:41 +0800

    练习题20

---
+++ src/com/zhongyu/hu/ValidParentheses.java
@@ -0,0 +1,68 @@
+package com.zhongyu.hu;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ * 20. Valid Parentheses
+ *
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Note that an empty string is also considered valid.
+ *
+ * Example 1:
+ *
+ * Input: "()"
+ * Output: true
+ * Example 2:
+ *
+ * Input: "()[]{}"
+ * Output: true
+ * Example 3:
+ *
+ * Input: "(]"
+ * Output: false
+ * Example 4:
+ *
+ * Input: "([)]"
+ * Output: false
+ * Example 5:
+ *
+ * Input: "{[]}"
+ * Output: true
+ *
+ */
+
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+
+            if (map.keySet().contains(curr)) {
+                stack.push(curr);
+            } else if (map.values().contains(curr)) {
+                if (!stack.empty() && map.get(stack.peek()) == curr) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.empty();
+    }
+}
