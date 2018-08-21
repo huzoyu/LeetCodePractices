95b0e66ae7019b67f41d7884ce473189ddb5297d
huzhongyu <noahahahah@foxmail.com>
2018-08-21 20:58:50 +0800

    练习题513

---
+++ src/com/zhongyu/hu/FindBottomLeftTreeValue.java
@@ -0,0 +1,48 @@
+package com.zhongyu.hu;
+
+/**
+ *
+ * 513. Find Bottom Left Tree Value
+ *
+ * Given a binary tree, find the leftmost value in the last row of the tree.
+ *
+ * Example 1:
+ * Input:
+ *
+ *     2
+ *    / \
+ *   1   3
+ *
+ * Output:
+ * 1
+ * Example 2:
+ * Input:
+ *
+ *         1
+ *        / \
+ *       2   3
+ *      /   / \
+ *     4   5   6
+ *        /
+ *       7
+ *
+ * Output:
+ * 7
+ * Note: You may assume the tree (i.e., the given root node) is not NULL.
+ */
+
+
+
+
+public class FindBottomLeftTreeValue {
+
+    public int findBottomLeftValue(TreeNode root) {
+        return findBottomLeftValue(root, 1, new int[]{0,0});
+    }
+    public int findBottomLeftValue(TreeNode root, int depth, int[] res) {
+        if (res[1]<depth) {res[0]=root.val;res[1]=depth;}
+        if (root.left!=null) findBottomLeftValue(root.left, depth+1, res);
+        if (root.right!=null) findBottomLeftValue(root.right, depth+1, res);
+        return res[0];
+    }
+}
